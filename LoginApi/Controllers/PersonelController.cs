0682ea91c163afade938193859ad490a70a8fd2e
Serkan Kocaman <kocaman.serkan98@gmail.com>
2021-03-24 11:55:17 +0300

    add

---
+++ LoginApi/Controllers/PersonelController.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace LoginApi.Controllers
+{
+
+    public class PersonelController : ApiController
+    {
+        serkanISGEntities db = new serkanISGEntities();
+        public HttpResponseMessage Get()
+        {
+
+            return Request.CreateResponse(HttpStatusCode.OK, db.PERSONEL.ToList());
+
+
+        }
+
+        public HttpResponseMessage Post(Giris gr)
+        {
+            PERSONEL person = new PERSONEL();
+
+            person = db.PERSONEL.FirstOrDefault(i => i.KULLANICI_ADI == gr.nickname);
+
+            if (person != null && person.SIFRE ==gr.password)
+            {
+             
+                return Request.CreateResponse(HttpStatusCode.OK, person);
+
+
+
+
+
+
+                /// burayı dolduracaktın personel nesnesinde ajaxı yazarken dikkat li ol büyük ihtimaşşe orda patladı geçen
+                /// 
+            }
+            else
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.NotFound, "Kullanıcı bulunamadı");
+            }
+
+
+
+
+
+
+
+
+
+
+        }
+
+
+
+    }
+
+
+    public class Giris
+    {
+
+
+
+        public string nickname { get; set; }
+        public string password { get; set; }
+
+
+    }
+
+
+
+}
---
+++ LoginApi/Controllers/PersonelMvcController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LoginApi.Controllers
+{
+    public class PersonelMvcController : Controller
+    {
+        // GET: PersonelMvc
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
